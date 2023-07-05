86ebdf4acc1ed5d7a834cac2b5a43716672e4f13
unknown <ibrahimkhalil.mine@gmail.com>
2023-07-05 10:14:41 +0600

    cors

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function run() {
       .collection("latestEvent");
 
       app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Origin", "https://aust-cdc.vercel.app");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
       });
